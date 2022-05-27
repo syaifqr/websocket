9a61e26404349d585da4cdbf4fef3bf8b17c09f9
syaifqr <bimasyaiful2@gmail.com>
2022-05-27 16:47:42 +0700

    up

--- group_chat.php
+++ group_chat.php
@@ -508,6 +508,7 @@ echo "<input type='hidden' name='userId' id='userId' value='" . $_SESSION['id'] 
                         styleBox = 'bg-red-200 text-right ml-auto';
                         styleBox2 = 'ml-[150px]';
                         styleBox3 = 'text-right';
+                        styleBox4 = 'zzzz';
                         val['name'] = "Me";
                     } else {
                         styleBox = 'bg-green-200 text-left mr-auto';
